2644a59fcb40d9edf56e01b564ee903421f16977
WilddogOo <sermonte@ucm.es>
2018-06-26 18:47:21 +0200

    Add files via upload

--- KNN_next.py
+++ KNN_next.py
@@ -1,29 +1,41 @@
  #saco los puntos de data obtenidos en el take de sorted +1 Ej: n=17 en el take aparece el par (17,293) pues saco en 294
  #Hago la media artimetica de los mismos y obtengo la prediccion
 def KNN_Next(rdd,d,k):
- data=dRddv2(rdd, d)
+ data=KNN_Elements(rdd, d)
  zipped=rdd.zipWithIndex().map(lambda (x,y):(y,x))
  n=data.count()-1
+ #Obtengo la matriz de distancias
  matrix=distanceMatrix(data)
+ #Ordeno los datos de la matriz de distancias correspondientes a numero n
  sorted=getData(matrix,n).sortBy(lambda (x,y):y)
- knn=sorted.map(lambda (x,y):x).map(lambda x:adjust(n,list(x))).take(k)
- selected=zipped.filter(lambda (x,y):selectKnnValues(x,knn)).map(lambda (x,y):y).take(k)
+ #Obtengo los k indices correspondientes del RDD para la prediccion
+ knn=sorted.map(lambda (x,y):x).zipWithIndex().map(lambda (x,y):(y,adjust(n,list(x),d))).filter(lambda(x,y):x<k).map(lambda (x,y):y).collect()
+ distance=sorted.map(lambda (x,y):y).zipWithIndex().filter(lambda(x,y):x<k).map(lambda (x,y):y).collect()
+ selected=zipped.filter(lambda (x,y):selectKnnValues(x,knn)).map(lambda (x,y):y).collect()
+ #Falta la funcion de peso y estaria completo
+ #Devuelve la media aritmetica de los valores de K seleccionados
  return float(sum(selected)) / max(len(selected), 1)
 
+
  
+#Coje los elementos KNN elegidos por ID del RDD recibido
 def selectKnnValues(x,knn):
   if x in knn:
    return True
   else:
    return False
- 
- 
-def adjust(n,x):
- if x[0]!= n :
-  return x[0]+1
- else:
-  return x[1]+1
 
-d=3
+
+#Elije los D siguientes a los K elegidos
+def adjust(n,x,d):
+ if x[0]==n:
+  return x[1]+d
+ else:
+  return x[0]+d
+
+
+  
+Xpredict = spark.sparkContext.textFile("hdfs:///loudacre/kb/Weather.csv").mapPartitionsWithIndex(deleteHeader).map(lambda x:np.array(x.split(","))).map(lambda x: np.float(x[1]))
+d=5
 k=3  
 KNN_Next(Xpredict,d,k)
--- matrizDistancia.py
+++ matrizDistancia.py
@@ -11,6 +11,7 @@ def distanceMatrix(rdd):
  data=data.map(lambda (x,y):(x,euclidea_dist(y[0],y[1])))
  return data
 
+
  
 #Devuelve la distancia euclidea entre 2 vectores
 def euclidea_dist(x,y):
@@ -18,6 +19,9 @@ def euclidea_dist(x,y):
     for i in range(0, len(x) if len(x) > len(y) else len(y)):
         dist += (x[i] - y[i])**2
     return math.sqrt(dist)
+
+
+
 	
 #Genera un RDD en funcion de la D
 def KNN_Elements(rddInput, d):
@@ -26,12 +30,15 @@ def KNN_Elements(rddInput, d):
 	 rddList.append(rddInput)
 	 for index in range(1,d):
 	    	rddList.append(groupMapping(rddInput,index,d))
-	 return spark.sparkContext.union(rddList).groupByKey().filter(lambda (x,y) : len(y) == d).mapValues(list).map(lambda(x,y):(x,sorted(y))).map(lambda(x,y):(x,orderList(y)))
-	 
+	 return spark.sparkContext.union(rddList).groupByKey().filter(lambda (x,y) : len(y) == d).mapValues(list).map(lambda(x,y):(x,sorted(y))).map(lambda(x,y):(x,eraseOrder(y)))
+
+
+
 #Funcion de mapper: union de RDDs
 def groupMapping(rdd,index,d):
     return rdd.map(lambda (x,y) : (x - index,[y[0]+index,y[1]]))
-	
+
+
 
 #Funcion de mapper:elimina las IDs de orden
 def eraseOrder(x):
@@ -40,7 +47,8 @@ def eraseOrder(x):
    k.append(x[i][1])
   return k
   
- 
+
+  
  #Funcion de mapper:Genera una ID de orden
 def defineOrder(x,y):
  k=list()
@@ -48,6 +56,9 @@ def defineOrder(x,y):
  k.append(x)
  return k
 
+
+
+ 
  #Elimina la cabecera de un RDD de entrada si lo tiene. 
 def deleteHeader(idx, iter):
     output=[]
@@ -59,16 +70,11 @@ def deleteHeader(idx, iter):
         return(output[1:])
 
 
+
+		
 #Devuelve las distancias de n de la matriz de distancias		
 def getData(matrix,n):
  row=matrix.filter(lambda (x,y):x[0]==n)
  col=matrix.filter(lambda (x,y):x[1]==n)
  return row.union(col)
- 
- 
-Xpredict = spark.sparkContext.textFile("hdfs:///loudacre/kb/Weather.csv").mapPartitionsWithIndex(deleteHeader).map(lambda x:np.array(x.split(","))).map(lambda x: np.float(x[1]))
-d=3
-rddTratado=dRddv2(Xpredict,d)
-matrix=distanceMatrix(rddTratado)
-matrix.take(10)
-matrix.sortBy(lambda (x,y):y).collect()
+
