282b3837e9cc751fa209b04c237b04f4d877983a
WilddogOo <sermonte@ucm.es>
2018-07-02 20:53:14 +0200

    Add files via upload

--- KNN_next.py
+++ KNN_next.py
@@ -1,21 +1,28 @@
  #saco los puntos de data obtenidos en el take de sorted +1 Ej: n=17 en el take aparece el par (17,293) pues saco en 294
  #Hago la media artimetica de los mismos y obtengo la prediccion
 def KNN_Next(rdd,d,k,distance="Euclidean"):
+ rdd=rdd.zipWithIndex().map(lambda (x,y):(y,x))
  data=dRdd(rdd, d)
- zipped=rdd.zipWithIndex().map(lambda (x,y):(y,x))
  n=data.count()-1
  #Obtengo la matriz de distancias
  matrix=distanceMatrix(data,distance)
+ data.unpersist()
+ matrix.cache()
  #Ordeno los datos de la matriz de distancias correspondientes a numero n
  sorted=getData(matrix,n).sortBy(lambda (x,y):y)
+ matrix.unpersist()
+ sorted.cache()
  #Obtengo los k indices correspondientes del RDD para la prediccion
- knn=sorted.map(lambda (x,y):x).zipWithIndex().map(lambda (x,y):(y,adjust(n,list(x),d))).filter(lambda(x,y):x<k).map(lambda (x,y):y).collect()
+ knn=sorted.map(lambda (x,y):x).zipWithUniqueId().map(lambda (x,y):(y,adjust(n,list(x),d))).filter(lambda(x,y):x<k).map(lambda (x,y):y).take(k)
+ sorted.unpersist()
  #distance=sorted.map(lambda (x,y):y).zipWithIndex().filter(lambda(x,y):x<k).map(lambda (x,y):y).collect()
- selected=zipped.filter(lambda (x,y):selectKnnValues(x,knn)).map(lambda (x,y):y).collect()
+ selected=rdd.filter(lambda (x,y):selectKnnValues(x,knn)).map(lambda (x,y):y).collect()
  #Falta la funcion de peso y estaria completo!!!!!!!!
  #Devuelve la media aritmetica de los valores de K seleccionados
- return groupLineData(selected)
-
+ if k>1:
+  return groupLineData(selected)
+ else:
+  return selected
 
  
 #Coje los elementos KNN elegidos por ID del RDD recibido
@@ -35,7 +42,7 @@ def adjust(n,x,d):
 
 
   
-Xpredict = spark.sparkContext.textFile("hdfs:///loudacre/kb/sea_ice.csv").mapPartitionsWithIndex(deleteHeader)
+Xpredict = spark.sparkContext.textFile("hdfs:///loudacre/kb/sunspot.month.csv").mapPartitionsWithIndex(deleteHeader).cache()
 d=5
 k=3  
 #Puedes pasar por parametro el tipo d distancia que quieras Manhattan,Euclidean y Canberra. Por defecto sera Euclidean
